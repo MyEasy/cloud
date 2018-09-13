fc62e742da9a4fdd587a78aad3caa70370274df8
MyEasy <2378075120@qq.com>
2018-09-13 09:48:07 +0800

    this is first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <my-header></my-header>
+    <!-- <img src="./assets/logo.png"> -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import myHeader from './components/header'
+export default {
+  name: 'App',
+  components: {myHeader}
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/header.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="sc-header">
+    <div class="sc-header-left">
+      <i class="iconfont icon-yunguantubiao-yuanjiao_aliyun sc-header-icon"></i>
+    </div>
+    <div class="sc-header-left2 sc-header-padding">
+      <span class="sc-header-fontsize">管理控制台</span>
+    </div>
+    <div class="sc-header-right1 sc-header-padding">
+      <i class="el-icon-search"></i>
+      <input class="sc-header-search" type="text" placeholder="搜索">
+    </div>
+  </div>
+</template>
+<script>
+</script>
+<style scoped>
+  *{
+    color: #ffffff;
+  }
+  .sc-header{
+    height: 50px;
+    width: 100%;
+    left: 0;
+    top: 0;
+    position: fixed;
+    background-color: #252a2f;
+  }
+  .sc-header-left{
+    float: left;
+    width: 50px;
+    height: 50px;
+    text-align: center;
+    line-height: 50px;
+  }
+  .sc-header-icon{
+    color: #ffffff;
+  }
+  .sc-header-left2{
+    float: left;
+    height: 50px;
+    line-height: 50px;
+  }
+  .sc-header-fontsize{
+    font-size: 14px
+  }
+  .sc-header-padding{
+    padding: 0 12px;
+  }
+  .sc-header-right1{
+    float: right;
+    height: 50px;
+    width: 200px;
+    line-height: 50px;
+  }
+  .sc-header-search{
+    background-color: #404448;
+    outline: none;
+    border: 0px;
+    height: 26px;
+    padding-left: 0px;
+  }
+</style>
